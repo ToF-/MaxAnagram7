085ade0d4b711fb92a17d65c2c94776fcc831cae
Christophe Thibaut <cthibauttof@gmail.com>
2025-08-01 17:58:54 +0200

    descending and remove next

--- MaxAnagram7/src/MaxAnagram7.hs
+++ MaxAnagram7/src/MaxAnagram7.hs
@@ -24,13 +24,27 @@ nextAnagram n | n < 100 =
     let n' = toNumber $ reverse $ fromNumber n
      in if n' < n then Just n' else Nothing
 nextAnagram n | n < 1000 =
-    let p = n `div` 100
-        s = n `mod` 100
-        u = n `mod` 10
-        v = n `div` 10
-    in case nextAnagram s of
-         Just t -> Just $ p * 100 + t
-         Nothing -> Just $ u * 100 + maxAnagram v
+    let digits = fromNumber n
+        suffix = descending digits
+    let prefix = n `div` 100
+        suffix = n `mod` 100
+        unity = n `mod` 10
+        newSuffix = n `div` 10
+    in case nextAnagram suffix of
+         Just nextSuffix -> Just $ prefix * 100 + nextSuffix
+         Nothing -> Just $ unity * 100 + maxAnagram newSuffix
+
+descending :: [Integer] -> [Integer] -> [Integer]
+descending [] result = result
+descending [x] result = x:result
+descending (x:y:xs) result | y > x = x:result
+descending (x:y:xs) result = descending (y:xs) x:result
+
+removeNext :: [Integer] -> Integer -> (Integer,[Integer]) -> (Integer,[Integer])
+removeNext [] _ (r,ys) = (r, ys)
+removeNext (x:xs) p (r,ys) | x >= p = removeNext xs p (r,x:ys)
+removeNext (x:xs) p (r,ys) | x <= r = removeNext xs p (r,x:ys)
+removeNext (x:xs) p (r,ys) | x > r  = removeNext xs p (x,r:ys)
 
 
 nextAnagram 8740 = Just 8470
@@ -41,5 +55,8 @@ fromNumber = digits 10
 toNumber :: [Integer] -> Integer
 toNumber = unDigits 10
 
+-- 8740
+-- 8740  0 < 4 → 87↔04 → 8704
+-- 8704  4,0 0 < 7 8↔ max(swap [4,0] 7) → 8470
 
 
